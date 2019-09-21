303a2d827edd992427b9be5a8582ebc7bcbada7c
ProgramistaZpolski <ProgramistaZpolski@users.noreply.github.com>
2019-09-21 21:02:07 +0200

    dodano source code

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace NowaEraDK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semilight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nowa Era Development Kit";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Segoe UI Semilight", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioButton1.Location = new System.Drawing.Point(99, 79);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(129, 34);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Decompile";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "What to do?";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Segoe UI Semilight", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioButton2.Location = new System.Drawing.Point(99, 119);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(107, 34);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Compile";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(386, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Proceed";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 240);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(368, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(368, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Version 0.1 Infdev";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 275);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Nowa Era Developer Kit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NowaEraDK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void DecNE()
+        {
+            
+            string filename;
+            string[] filePaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory+"\\app\\data\\game\\");
+            foreach (string myfile in filePaths)
+            {
+                filename = Path.ChangeExtension(myfile, ".swf");
+                System.IO.File.Move(myfile, filename);
+            }
+            System.IO.File.Move(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\game\\video_8_4.swf", AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\game\\video_8_4.flv");
+            filePaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\lib\\");
+            foreach (string myfile in filePaths)
+            {
+                filename = Path.ChangeExtension(myfile, ".swf");
+                System.IO.File.Move(myfile, filename);
+            }
+            filePaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\video\\");
+            foreach (string myfile in filePaths)
+            {
+                filename = Path.ChangeExtension(myfile, ".png");
+                System.IO.File.Move(myfile, filename);
+            }
+        }
+        public void ComNE()
+        {
+
+            string filename;
+            string[] filePaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\game\\");
+            foreach (string myfile in filePaths)
+            {
+                filename = Path.ChangeExtension(myfile, ".ne");
+                System.IO.File.Move(myfile, filename);
+            }
+            System.IO.File.Move(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\game\\video_8_4.ne", AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\game\\video_8_4.ne");
+            filePaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\lib\\");
+            foreach (string myfile in filePaths)
+            {
+                filename = Path.ChangeExtension(myfile, ".ne");
+                System.IO.File.Move(myfile, filename);
+            }
+            filePaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\video\\");
+            foreach (string myfile in filePaths)
+            {
+                filename = Path.ChangeExtension(myfile, ".ne");
+                System.IO.File.Move(myfile, filename);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                DecNE();
+            }
+            else
+            {
+                ComNE();
+            }
+        }
+    }
+}
