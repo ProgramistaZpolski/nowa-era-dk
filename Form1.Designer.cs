78af58a5a9219e21b67e67c832653b326d8e367a
ProgramistaZpolski <ProgramistaZpolski@users.noreply.github.com>
2019-09-22 13:07:24 +0200

    naprawiono progressbar

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,7 +41,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semilight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(69, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(330, 37);
             this.label1.TabIndex = 0;
@@ -51,7 +51,7 @@
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Font = new System.Drawing.Font("Segoe UI Semilight", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.radioButton1.Location = new System.Drawing.Point(99, 79);
+            this.radioButton1.Location = new System.Drawing.Point(153, 79);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(129, 34);
             this.radioButton1.TabIndex = 1;
@@ -62,7 +62,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 50);
+            this.label2.Location = new System.Drawing.Point(189, 46);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(66, 13);
             this.label2.TabIndex = 2;
@@ -72,7 +72,7 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Font = new System.Drawing.Font("Segoe UI Semilight", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.radioButton2.Location = new System.Drawing.Point(99, 119);
+            this.radioButton2.Location = new System.Drawing.Point(153, 119);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(107, 34);
             this.radioButton2.TabIndex = 3;
@@ -82,7 +82,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(386, 240);
+            this.button1.Location = new System.Drawing.Point(386, 195);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -92,7 +92,7 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 240);
+            this.progressBar1.Location = new System.Drawing.Point(12, 195);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(368, 23);
             this.progressBar1.TabIndex = 5;
@@ -100,17 +100,17 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(368, 224);
+            this.label3.Location = new System.Drawing.Point(359, 179);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.Size = new System.Drawing.Size(102, 13);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Version 0.1 Infdev";
+            this.label3.Text = "Version 0.1.1 Infdev";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 275);
+            this.ClientSize = new System.Drawing.Size(473, 227);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
--- Form1.cs
+++ Form1.cs
@@ -27,6 +27,7 @@ namespace NowaEraDK
                 filename = Path.ChangeExtension(myfile, ".swf");
                 System.IO.File.Move(myfile, filename);
             }
+            progressBar1.Value += 65;
             System.IO.File.Move(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\game\\video_8_4.swf", AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\game\\video_8_4.flv");
             filePaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\lib\\");
             foreach (string myfile in filePaths)
@@ -34,12 +35,14 @@ namespace NowaEraDK
                 filename = Path.ChangeExtension(myfile, ".swf");
                 System.IO.File.Move(myfile, filename);
             }
+            progressBar1.Value += 30;
             filePaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\video\\");
             foreach (string myfile in filePaths)
             {
                 filename = Path.ChangeExtension(myfile, ".png");
                 System.IO.File.Move(myfile, filename);
             }
+            progressBar1.Value = 100;
         }
         public void ComNE()
         {
@@ -51,6 +54,7 @@ namespace NowaEraDK
                 filename = Path.ChangeExtension(myfile, ".ne");
                 System.IO.File.Move(myfile, filename);
             }
+            progressBar1.Value = 50;
             System.IO.File.Move(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\game\\video_8_4.ne", AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\game\\video_8_4.ne");
             filePaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\lib\\");
             foreach (string myfile in filePaths)
@@ -58,12 +62,14 @@ namespace NowaEraDK
                 filename = Path.ChangeExtension(myfile, ".ne");
                 System.IO.File.Move(myfile, filename);
             }
+            progressBar1.Value = 75;
             filePaths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\app\\data\\video\\");
             foreach (string myfile in filePaths)
             {
                 filename = Path.ChangeExtension(myfile, ".ne");
                 System.IO.File.Move(myfile, filename);
             }
+            progressBar1.Value = 100;
         }
 
         private void Button1_Click(object sender, EventArgs e)
